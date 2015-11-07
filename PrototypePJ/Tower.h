d527c235fc595159be2893dfbc37842781eea506
hwkim25 <hwkim25@postech.ac.kr>
2015-11-07 15:48:35 +0900

    BattleObject Update

--- PrototypePJ/Tower.h
+++ PrototypePJ/Tower.h
@@ -24,7 +24,9 @@ class Tower: public BattleObject{
 public:
     Tower();
     void upgrade(Upgrades, int);                    //이 method는 slow, gold가 적용되는 하위상속 tower에서는 overriding할 계획 일단 공격력, 방어력, 공속에 대해 구현함
-    void fuseTower(Tower*, Tower*);
+    void fuseTower(Tower*, Tower*);                 //합성법 참고해서 만들기
+    int GetTargetNum();
+    int GetTowerCode();
 protected:
     int TargetNum;
     int TowerCode;
