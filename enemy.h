1392be04c2f41aef404b4183ebceb7847abd7d25
ycjungSubhuman <jung3519@gmail.com>
2015-11-14 16:43:36 +0900

    11/14Merged
    Merged

---
+++ enemy.h
@@ -0,0 +1,24 @@
+#ifndef ENEMY_H
+#define ENEMY_H
+
+#include <QGraphicsRectItem>
+#include <QObject>
+#include<QWidget>
+
+class Enemy : public QObject, public QGraphicsRectItem{
+Q_OBJECT
+private:
+    float path[10][2];
+    int currentPath;
+    int clockRate;
+    float x_move;
+    float y_move;
+public:
+    Enemy(int level);
+public slots:
+    void move();
+};
+
+
+#endif // ENEMY_H
+
