6c209d5d00ca9701b2cbdfa33bbccf1558435c32
ycjungSubhuman <jung3519@gmail.com>
2015-11-22 18:59:49 +0900

    requested methods added in enemy class /change by dasom.

--- include/enemy.h
+++ include/enemy.h
@@ -4,19 +4,35 @@
 #include <QGraphicsRectItem>
 #include <QObject>
 #include<QWidget>
+#include <BattleObject.h>
+#include <QTimer>
 
-class Enemy : public QObject, public QGraphicsRectItem{
+class Enemy : public BattleObject{
 Q_OBJECT
 private:
-    float path[10][2];
-    int currentPath;
+    QTimer* timer;          //path
+    QTimer* poisonTime;     //poisoned->recovered
+    QTimer* slowTime;       //slowed->recovered
+    float path[10][2];      //x,y
+    int currentPath;        //show current path
     int clockRate;
-    float x_move;
-    float y_move;
+    float x_move;           //move in one clock
+    float y_move;           //move in one clock
+    int currentLevel;       //level
+    void setPicture();      //according to level set different enemy's picture
+    int life;               //DiedOrNot
+    float poisonedTime;     //elapsed time from poisoning point
+    int slowedState;        //slowed:1, not:0
 public:
-    Enemy(int level=1);
+    Enemy(int level=1);               //constructor
+    int DieOrNot(){ return life; }    //Die=0, not=1
+    void IsPoisonedBy(int);           //called when attack by poison tower
+    void IsSlowedBy();                //called when attack by slow tower
+    void IsHitBy(int);               //called when attacked
 public slots:
-    void move();
+    void move();                      //go through path
+    void IsHitByP(int);               //decrease Hp, when poisoned by tower
+    void changeClockRate();           //change the move speed of enemy
 };
 
 
--- src/enemy.cpp
+++ src/enemy.cpp
@@ -1,15 +1,25 @@
 #include "enemy.h"
-#include <QTimer>
 #include <QDebug>
 #include <QGraphicsScene>
 
 #define width 500
 #define height 500
 
+
+
 Enemy::Enemy(int level)
 {
+    currentLevel = level;
+
+    int xLoc[10] = {70,45,20,20,50,80,80,80,45,20};
+    int yLoc[10] = {80,80,80,60,60,60,35,10,10,10};
+
     qDebug() << "make enemy";
-    path[0][0]=70*width/100;           //path represented by points 0->1.....
+    for(int i=0;i<10;i++){
+        path[i][0] = xLoc[i]*width/100;
+        path[i][1] = yLoc[i]*height/100;
+    }
+    /*path[0][0]=70*width/100;           //path represented by points 0->1.....
     path[0][1]=80*height/100;
     path[1][0]=45*width/100;
     path[1][1]=80*height/100;
@@ -28,19 +38,74 @@ Enemy::Enemy(int level)
     path[8][0]=45*width/100;
     path[8][1]=10*height/100;
     path[9][0]=20*width/100;
-    path[9][1]=10*height/100;
-
+    path[9][1]=10*height/100; */
     currentPath=-1;     //not yet on path
 
-    clockRate=10000/(60+level);   //different velocity according to level
+    clockRate=10000/(60+currentLevel);   //different velocity according to level
 
-    setRect(0,0,width/10,height/10);
+    life=1;
+    Hp=(currentLevel/10+1)*1000;
+    DefensivePower=10;
+
+    setPicture();
+
     setPos(path[0][0],path[0][1]);
 
     //move according to signal
-    QTimer* timer = new QTimer();
+    timer = new QTimer();
     connect(timer, SIGNAL(timeout()), this, SLOT(move()));
     timer->start(clockRate);
+}
+
+void Enemy::IsPoisonedBy(int power)
+{
+    poisonedTime=0;
+    poisonTime = new QTimer();
+    connect(timer, SIGNAL(timeout()), this, SLOT(IsHitByP(power)));
+    timer->start(clockRate);
+}
+
+void Enemy::IsSlowedBy()
+{
+    slowedState=0;
+    slowTime = new QTimer();
+    connect(timer, SIGNAL(timeout()), this, SLOT(changeClockRate()));
+    timer->start(1000);
+}
+
+void Enemy::IsHitBy(int power)
+{
+      BattleObject::IsHitBy(power);   //decrease Hp
+      if(Hp<=0){
+          scene()->removeItem(this);
+          delete timer;
+      }
+
+}
+
+void Enemy::setPicture()     //Hp AttackPower DefensePower
+{
+    int n = currentLevel;
+
+    switch(n/10){
+    case 0:
+        if(n%10) setPixmap(QPixmap(":/images/Enemy1.png"));
+        else     setPixmap(QPixmap(":/images/Enemy2.png"));
+        break;
+    case 1:
+        if(n%10) setPixmap(QPixmap(":/images/Enemy3.png"));
+        else     setPixmap(QPixmap(":/images/Enemy4.png"));
+        break;
+    case 2:
+        if(n%10) setPixmap(QPixmap(":/images/Enemy5.png"));
+        else     setPixmap(QPixmap(":/images/Enemy6.png"));
+        break;
+    case 3:
+        if(n%10) setPixmap(QPixmap(":/images/Enemy7.png"));
+        else     setPixmap(QPixmap(":/images/Enemy8.png"));
+        break;
+    }
+
 }
 
 void Enemy::move()
@@ -53,10 +118,42 @@ void Enemy::move()
 
     setPos(x()+x_move, y()+y_move);
 
-    if((x()== path[9][0]) && (y()==path[9][1])){
+    if((x()== path[9][0]) && (y()==path[9][1])){                 //end point
         qDebug() << "Fail to remove enemy";
         scene()->removeItem(this);
-        delete this;
+        delete timer;
+    }
+
+}
+
+void Enemy::IsHitByP(int power)     //poisoned
+{
+    BattleObject::IsHitBy(power);   //decrease Hp
+    poisonedTime+=clockRate;        //calculate poisoned time
+
+    if(Hp<=0){
+        delete poisonTime;
+        scene()->removeItem(this);
+        delete timer;
+    }
+
+    if(poisonedTime>500)            //after specific time, released from poison
+        delete poisonTime;
+}
+
+void Enemy::changeClockRate()
+{
+    delete timer;
+    timer = new QTimer();
+    connect(timer, SIGNAL(timeout()), this, SLOT(move()));
+    if (slowedState=0){
+        timer->start(2*clockRate);    //slowed, doubled clockrate
+        slowedState=1;
+    }
+    else {
+        timer->start(clockRate);      //recovered from slow state
+        slowedState=0;
+        delete slowTime;
     }
 
 }
